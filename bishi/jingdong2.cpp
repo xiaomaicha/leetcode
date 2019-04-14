3a89ce6f093e2dfd36b9cb10ea1c3304073d8181
xiaomaicha <31534343+xiaomaicha@users.noreply.github.com>
2019-04-14 11:06:01 +0800

    jingdong

---
+++ bishi/jingdong2.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Administrator on 2019/4/13.
+//
+
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void substr_set_op(string strt, vector<string> out, vector<vector<string>>& res){
+    if(strt.empty()){
+        res.push_back(out);
+        return;
+    } else{
+        for(unsigned int i=1; i<=strt.size();++i){
+//            if(i==0&&strt.size()>=2)
+//                substr_set_op(strt.substr(i+1), out, res);
+            string substr = strt.substr(0,i);
+            out.push_back(substr);
+            substr_set_op(strt.substr(i), out, res);
+            out.pop_back();
+        }
+    }
+}
+
+int main(){
+    unsigned int m;
+    vector<string> strs;
+    string strt;
+    cin>>m;
+    cin.get();
+    for (int i = 0; i < m; ++i) {
+        string nums;
+        getline(cin,nums);
+        strs.push_back(nums);
+    }
+    cin.get();
+    getline(cin,strt);
+
+//    string sub = strt.substr(0,0);
+
+    vector<vector<string>> substr_set;
+    vector<string> substr;
+    substr_set_op(strt, substr,substr_set);
+
+    cout<<endl;
+
+
+    return 0;
+
+}
+
+
